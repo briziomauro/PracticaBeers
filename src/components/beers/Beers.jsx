72ab850e5a645055a1dc9b97f2005709c2de93be
maurobrizio <briziomauro2003@gmail.com>
2024-04-13 17:31:18 -0300

    Pulido

--- src/components/beers/Beers.jsx
+++ src/components/beers/Beers.jsx
@@ -1,11 +1,7 @@
-import React, { useState } from 'react'
-import { beers } from '../data/Data'
-import { Col, Container, ListGroup, Row } from 'react-bootstrap';
+import React from 'react'
 import Available from '../available/Available';
 
 const Beers = () => {
-    const conversion = 1000;
-
     return (
         <>
             <Available />
